22be97ba6451f4b59e26d4912e244411c2dde8ae
88Hannah <hannahsangar@gmail.com>
2020-10-04 16:51:59 +0100

    IP address tracker complete

--- 05-IP-address-tracker/script.js
+++ 05-IP-address-tracker/script.js
@@ -5,56 +5,43 @@ const locationResult = document.querySelector('.location-result');
 const timezoneResult = document.querySelector('.timezone-result');
 const ispResult = document.querySelector('.isp-result');
 
-
 let currentIP = "";
 let newData = ""
-
-let success;
 let mymap;
-
 
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    getNewIP();
+    ipSearch();
 });
 
 
-const getNewIP = () => {
+const getNewIP = async () => {
     currentIP = ipInput.value;
-    retrieveIPInfo(currentIP);
+    await retrieveIPInfo(currentIP);
+    
 };
-
-
 
 
 const retrieveIPInfo = async ip => {
-
     const ipGeoKey = 'at_l1HbDiuaHTtAwMOUn8HdBLK5a202Z';
-    const ipAddress = ip;
 
-    await fetch(`https://geo.ipify.org/api/v1?apiKey=${ipGeoKey}&ipAddress=${ipAddress}`)
+    await fetch(`https://geo.ipify.org/api/v1?apiKey=${ipGeoKey}&ipAddress=${ip}`)
         .then(response => response.json())
         .then(data => {
             newData = data;
-            success = true;
-            console.log(newData);
+            if(newData.code === 422){
+                alert("This is not a valid IP address.");
+                ipInput.value = "";
+            } else {
+                
+            };
         })
         .catch(error => {
-            success = false;
+            alert("There was a problem retrieving the result, please try again.");
             console.log(error);
         });
-
-        if(!success) {
-            alert("it didn't work");
-        } else if (newData.code === 422){
-            alert("Wrong IP address");
-        };
-
-        populateFields(newData);
-
 };
-
 
 
 const populateFields = data => {
@@ -62,11 +49,7 @@ const populateFields = data => {
     locationResult.innerHTML = `${data.location.city}, ${data.location.region}`;
     timezoneResult.innerHTML = `UTC ${data.location.timezone}`;
     ispResult.innerHTML = data.isp;
-
-    displayMap(data.location);
-
 };
-
 
 
 const displayMap = (location) => {
@@ -75,23 +58,33 @@ const displayMap = (location) => {
     };
     const lat = location.lat;
     const long = location.lng;
-    mymap = L.map('mapid').setView([lat, long], 11);
-    var marker = L.marker([lat, long]).addTo(mymap);
+    mymap = L.map('mapid', {
+        center: [(lat - 0.02), long],
+        zoom: 11,
+        zoomControl: false
+    });
+    L.marker([lat, long]).addTo(mymap);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaGFubmFoODgiLCJhIjoiY2tmc2p1NWF2MDdzMjJxdDh5cHBkcWkydyJ9.vbXECHYRthHA6OxzOcLL9A'
-}).addTo(mymap);
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiaGFubmFoODgiLCJhIjoiY2tmc2p1NWF2MDdzMjJxdDh5cHBkcWkydyJ9.vbXECHYRthHA6OxzOcLL9A'
+    }).addTo(mymap);
 }
 
 
+const ipSearch = async () => {
+    await getNewIP();
+    populateFields(newData);
+    displayMap(newData.location);
+}
+
 
 // On first load
-const init = (async () => {
-    await retrieveIPInfo("");
+const init = (() => {
+    ipSearch();
 })();
 
 
